3f5f939081b10c5b7c70c732f250474af9593ac8
Eben <me@ebenroux.co.za>
2015-11-23 12:26:44 +0200

    - added IProcessHandlerContext

---
+++ Shuttle.ESB.Process/IProcessHandlerContext.cs
@@ -0,0 +1,11 @@
+﻿using Shuttle.ESB.Core;
+using Shuttle.Recall.Core;
+
+namespace Shuttle.ESB.Process
+{
+    public interface IProcessHandlerContext<out T> : IHandlerContext<T> where T : class
+    {
+        IKeyStore KeyStore { get; }
+        EventStream Stream { get; }
+    }
+}
