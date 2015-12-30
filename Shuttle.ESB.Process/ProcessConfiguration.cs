e679447640bde57f04e07df7b5c52725c6286105
Eben <me@ebenroux.co.za>
2015-12-30 20:53:02 +0200

    - v3.0.7

--- Shuttle.ESB.Process/ProcessConfiguration.cs
+++ Shuttle.ESB.Process/ProcessConfiguration.cs
@@ -4,13 +4,18 @@ namespace Shuttle.ESB.Process
 {
     public class ProcessConfiguration : IProcessConfiguration
     {
-        private static object Padlock = new object();
-        public string ProviderName { get; set; }
+        private static readonly object Padlock = new object();
+	    private IProcessActivator _processActivator;
+	    public string ProviderName { get; set; }
         public string ConnectionString { get; set; }
-        
-        public IProcessActivator ProcessActivator { get; set; }
 
-        private static T Synchronised<T>(Func<T> f)
+	    public IProcessActivator ProcessActivator
+	    {
+		    get { return _processActivator ?? Synchronised(()=> _processActivator = new DefaultProcessActivator()); }
+		    set { _processActivator = value; }
+	    }
+
+		private static T Synchronised<T>(Func<T> f)
         {
             lock (Padlock)
             {
