1af39c7643175b8dff51fb46e3d8a8f69e6a2771
Eben <me@ebenroux.co.za>
2017-07-01 22:14:55 +0200

    - added bootstrap

---
+++ Shuttle.Esb.Process/Bootstrap.cs
@@ -0,0 +1,47 @@
+﻿using Shuttle.Core.Infrastructure;
+
+namespace Shuttle.Esb.Process
+{
+	public class Bootstrap :
+		IComponentRegistryBootstrap,
+		IComponentResolverBootstrap
+	{
+		private static bool _registryBootstrapCalled;
+		private static bool _resolverBootstrapCalled;
+
+		public void Register(IComponentRegistry registry)
+		{
+			Guard.AgainstNull(registry, "registry");
+
+			if (_registryBootstrapCalled)
+			{
+				return;
+			}
+
+			registry.AttemptRegister<IProcessConfiguration>(ProcessSection.Configuration());
+			registry.AttemptRegister<IProcessActivator, DefaultProcessActivator>();
+			registry.AttemptRegister<IMessageHandlerInvoker, ProcessMessageHandlerInvoker>();
+
+			_registryBootstrapCalled = true;
+		}
+
+		public void Resolve(IComponentResolver resolver)
+		{
+			Guard.AgainstNull(resolver, "resolver");
+
+			if (_resolverBootstrapCalled)
+			{
+				return;
+			}
+
+			var processActivator = resolver.Resolve<IProcessActivator>() as DefaultProcessActivator;
+
+			if (processActivator != null)
+			{
+				processActivator.RegisterMappings();
+			}
+
+			_resolverBootstrapCalled = true;
+		}
+	}
+}
