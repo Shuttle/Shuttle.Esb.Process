a1963b19cb4e705759015c68c0e0051a93607cf7
Eben <me@ebenroux.co.za>
2019-06-21 17:35:54 +0200

    - v11.0.0

--- Shuttle.ESB.Process/Properties/AssemblyInfo.cs
+++ Shuttle.ESB.Process/Properties/AssemblyInfo.cs
@@ -5,22 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Framework 4.6.1")]
 #endif
 
-#if NET462
-[assembly: AssemblyTitle(".NET Framework 4.6.2")]
-#endif
-
-#if NET47
-[assembly: AssemblyTitle(".NET Framework 4.7")]
-#endif
-
-#if NET471
-[assembly: AssemblyTitle(".NET Framework 4.7.1")]
-#endif
-
-#if NETCOREAPP2_0
-[assembly: AssemblyTitle(".NET Core 2.0")]
-#endif
-
 #if NETCOREAPP2_1
 [assembly: AssemblyTitle(".NET Core 2.1")]
 #endif
@@ -29,10 +13,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.1.0")]
-[assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
+[assembly: AssemblyVersion("11.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2019")]
 [assembly: AssemblyProduct("Shuttle.Esb.Process")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.1")]
+[assembly: AssemblyInformationalVersion("11.0.0")]
 [assembly: ComVisible(false)]
