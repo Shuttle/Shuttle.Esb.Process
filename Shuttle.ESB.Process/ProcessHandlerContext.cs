61eee6ecfa99baf2265e4cc65e2cf431f80f13e2
Eben <me@ebenroux.co.za>
2020-01-23 20:33:02 +0200

    - nuspec

--- Shuttle.ESB.Process/ProcessHandlerContext.cs
+++ Shuttle.ESB.Process/ProcessHandlerContext.cs
@@ -7,7 +7,7 @@ namespace Shuttle.Esb.Process
 {
 	public class ProcessHandlerContext<T> : HandlerContext<T>, IProcessHandlerContext<T> where T : class
 	{
-		public ProcessHandlerContext(IServiceBusConfiguration configuration, ITransportMessageFactory transportMessageFactory,
+		public ProcessHandlerContext(ITransportMessageFactory transportMessageFactory,
 			IPipelineFactory pipelineFactory, ISubscriptionManager subscriptionManager, TransportMessage transportMessage,
 			T message, CancellationToken cancellationToken, EventStream stream) :
 			base(transportMessageFactory, pipelineFactory, subscriptionManager, transportMessage, message, cancellationToken)
