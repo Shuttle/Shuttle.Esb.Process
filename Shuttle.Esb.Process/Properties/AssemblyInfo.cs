485740a5323a7139d1e81e5e8c0b30a143b464a7
Eben <me@ebenroux.co.za>
2022-04-09 10:54:03 +0200

    - renamed ESB to Esb

---
+++ Shuttle.Esb.Process/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#if NET461
+[assembly: AssemblyTitle(".NET Framework 4.6.1")]
+#endif
+
+#if NETCOREAPP2_1
+[assembly: AssemblyTitle(".NET Core 2.1")]
+#endif
+
+#if NETSTANDARD2_0
+[assembly: AssemblyTitle(".NET Standard 2.0")]
+#endif
+
+[assembly: AssemblyVersion("12.0.1.0")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2019")]
+[assembly: AssemblyProduct("Shuttle.Esb.Process")]
+[assembly: AssemblyCompany("Shuttle")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyInformationalVersion("12.0.1")]
+[assembly: ComVisible(false)]
